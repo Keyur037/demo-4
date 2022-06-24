5cf06f5873b31ac1b554af0a8cbbd26f11e253a8
Keyur037 <107847481+Keyur037@users.noreply.github.com>
2022-06-24 17:38:06 +0530

    dictionary orientaion fileuploaded

---
+++ dictionary_oerations.py
@@ -0,0 +1,21 @@
+people={'Arham':'Blue','Lisa':'Yellow','Vinod':'Purple','Jenny':'Pink'}
+k=[]
+
+#print total no. of student
+print("no. of student in list is ",len(people))
+
+#change favorite color of Lisa
+people['Lisa']='White'
+print("Lisa favorite color is changed to",people['Lisa'])
+del people['Jenny']
+print(people)
+
+
+#sort student name
+for i in people.keys():
+    k.append(i)
+k.sort()
+
+#print student name
+for o in k:
+    print(o,"favorite color is",people[o])
